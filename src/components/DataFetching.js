4f9606c6d4bdf16cd5ffb76d5b9e98c6740e17bc
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-07-08 03:27:02 +0430

    post method

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -1,22 +1,25 @@
 import React, { useState,useEffect } from 'react'
 import axios from 'axios';
 function DataFetching(){
-  const [posts,setPosts] = useState([]);
+  const [post,setPost] = useState({});
+  const [id,setId] = useState(1);
   useEffect(()=>{
-    axios.get("https://jsonplaceholder.typicode.com/posts")
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
     .then(res => {
-      setPosts(res.data)
+      setPost(res.data)
     })
     .catch(err=> console.log(err))
-  },[])
+  },[id])
 return <div>
-<ul>
+<input type='text'  value={id} onChange={e=> setId(e.target.value)}/>
+<div>{post.title}</div>
+{/*<ul>
   {
-posts.map((post)=>
+post.map((post)=>
 <li key={post.id}>{post.title}</li>
 )
   }
-</ul>
+</ul>*/}
 </div>
 }
 export default DataFetching;
